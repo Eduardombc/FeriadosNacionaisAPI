a75213dd2e316b021518d48db4a76398433f2558
Eduardombc <108088699+Eduardombc@users.noreply.github.com>
2025-06-14 11:36:50 -0300

    Inicio do Projeto

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+//usado apenas class e não public class para manter o escopo local
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Cria uma instância de HttpClient
+        var httpClient = new HttpClient();
+        // Pega feriados nacionais do Brasil de 2025 e salva dentro da variável json
+        string json = await httpClient.GetStringAsync("https://date.nager.at/api/v3/PublicHolidays/2025/BR");
+        // Para deserializar o JSON, usamos a biblioteca System.Text.Json
+        var feriados = JsonSerializer.Deserialize<List<Feriado>>(json);
+
+        // Exibe os feriados
+        foreach (var feriado in feriados)
+        {
+            Console.WriteLine($"Data: {feriado.Date}, Nome Local: {feriado.LocalName}, Nome: {feriado.Name}, Código do País: {feriado.CountryCode}, Fixo: {feriado.Fixed}, Global: {feriado.Global}, Condados: {feriado.Counties}, Ano de Lançamento: {feriado.LaunchYear}, Tipo: {feriado.Type}");
+        }
+        // Aguarda a entrada do usuário antes de fechar o console
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
