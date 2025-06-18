774b7ab0564817cb8b78c1bcd9ff754667d820d4
Eduardombc <108088699+Eduardombc@users.noreply.github.com>
2025-06-18 09:26:53 -0300

    Adição de pesquisa em cache

--- Api/Api/Controllers/FeriadosController.cs
+++ Api/Api/Controllers/FeriadosController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
+using FeriadosNacionaisAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace FeriadosNacionaisAPI.Api.Controllers
 {
@@ -13,11 +16,15 @@ namespace FeriadosNacionaisAPI.Api.Controllers
         // Campo privado para armazenar a instância de HttpClient
         private readonly HttpClient _httpClient;
 
+        // Campo privado para armazenar o contexto do banco de dados
+        private readonly FeriadoDbContext _context;
+
         // Construtor que recebe um HttpClient através da injeção de dependência
-        public FeriadosController(HttpClient httpClient)
+        public FeriadosController(HttpClient httpClient, FeriadoDbContext context)
         {
             // Inicializa o HttpClient através da injeção de dependência
             _httpClient = httpClient;
+            _context = context;
         }
         [HttpGet]
         // Define o método HTTP GET para obter a lista de feriados
@@ -26,6 +33,14 @@ namespace FeriadosNacionaisAPI.Api.Controllers
         // - ActionResult: Representa o resultado de uma ação em um controlador ASP.NET Core, permitindo retornar diferentes tipos de respostas HTTP.
         public async Task<ActionResult<List<Feriado>>> GetFeriadosAsync()
         {
+            if (await _context.Feriados.AnyAsync())
+            {
+                // Se já existem feriados no banco de dados, retorna a lista de feriados
+                var feriadosdoDB = await _context.Feriados.ToListAsync();
+
+                // Retorna um resultado HTTP 200 OK com a lista de feriados do banco de dados
+                return Ok(feriadosdoDB);
+            }
             // Pega feriados nacionais do Brasil de 2025 e salva dentro da variável json
             string json = await _httpClient.GetStringAsync("https://date.nager.at/api/v3/PublicHolidays/2025/BR");
 
@@ -40,10 +55,20 @@ namespace FeriadosNacionaisAPI.Api.Controllers
                 PropertyNameCaseInsensitive = true // Permite que as propriedades sejam lidas sem diferenciar maiúsculas de minúsculas
             };
             // Para deserializar o JSON, usamos a biblioteca System.Text.Json
-            var feriados = JsonSerializer.Deserialize<List<Feriado>>(json, options);
+            var feriadosdaAPI = JsonSerializer.Deserialize<List<Feriado>>(json, options);
             // - Ok: Retorna um resultado HTTP 200 OK com a lista de feriados.
-            return Ok(feriados);
+            
+            if (feriadosdaAPI != null && feriadosdaAPI.Any())
+            {
+                // Se a lista de feriados não for nula e contiver elementos, adiciona os feriados ao banco de dados
+                await _context.Feriados.AddRangeAsync(feriadosdaAPI);
+                // Salva as alterações no contexto do banco de dados
+                await _context.SaveChangesAsync();
+            }
 
+
+            // Adiciona os feriados ao banco de dados
+            return Ok(feriadosdaAPI);
         }
     }
 }
